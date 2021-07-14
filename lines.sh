4867e325f4c63b3520fef6efdac0ec7765501fc0
Avinash Devina <mail4uavi@gmail.com>
2021-07-14 13:52:05 +0530

    Test

--- lines.sh
+++ lines.sh
@@ -38,7 +38,7 @@ tput sgr0
 
 echo -e "\e[36;1m bold"
      echo -e "\e[30;1m black"
-     echo -e "\e[31m red"
+     echo -e "red"
      echo -e "\e[32m green"
      echo -e "\e[33m yellow"
      echo -e "\e[34m blue"
