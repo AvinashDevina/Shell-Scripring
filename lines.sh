84436951d3b2046028414e52ffa02d0a89ec7301
Avinash Devina <mail4uavi@gmail.com>
2021-07-14 13:03:59 +0530

    Test

--- lines.sh
+++ lines.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo -e "\n\n\t\t\e[31mhello\e[0m \n\n\t\t\n\t\t\t\e[33mWorld\e[0m"
+echo -e "\033\n\n\t\t\e[31mhello\e[0m \n\n\t\t\n\t\t\t\e[33mWorld\e[0m"
 
 
 echo -e "\e[35mAVINASH\e[0m"
