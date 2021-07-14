32aebfe23facb270b29f61e796542b7aad0473fc
Avinash Devina <mail4uavi@gmail.com>
2021-07-14 13:46:47 +0530

    Test

--- lines.sh
+++ lines.sh
@@ -14,5 +14,23 @@ echo -e "\033[0;37mAVINASH"
 
 
 
-echo -e "\n\n\t\033[1;1;32mAmma\e[0m \n\n\t\033[1;34mnanna\e[0m"
+echo -e "\n\n\t\033[1;32mAmma\e[0m \n\n\t\033[1;34mnanna\e[0m"
 echo -e "\n\n\t\033[0;32mAmma\e[0m \n\n\t\033[0;34mnanna\e[0m"
+
+
+#!/bin/bash
+PATH=/bin:/usr/bin:
+
+NONE='\033[00m'
+RED='\033[01;31m'
+GREEN='\033[01;32m'
+YELLOW='\033[01;33m'
+PURPLE='\033[01;35m'
+CYAN='\033[01;36m'
+WHITE='\033[01;37m'
+BOLD='\033[1m'
+UNDERLINE='\033[4m'
+
+echo -e "This text is ${RED}red${NONE} and ${GREEN}green${NONE} and ${BOLD}bold${NONE} and ${UNDERLINE}underlined${NONE}."
+
+tput sgr0
