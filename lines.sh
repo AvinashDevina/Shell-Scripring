fb22fba1cbc2acaf58e4b00b04a0f22b6c7c135c
Avinash Devina <mail4uavi@gmail.com>
2021-07-14 12:46:24 +0530

    Test

--- lines.sh
+++ lines.sh
@@ -3,8 +3,8 @@
 echo -e "\n\n\t\t\e[31mhello\e[0m \n\n\t\t\n\t\t\t\e[33mWorld\e[0m"
 
 
-echo -e "\e[35mAVINASH\e[0m"
+echo -e "\e[35m1mAVINASH\e[0m"
 
 
-echo -e '\033[1mYOUR_STRING\033[0m'
+echo -e '\033[mYOUR_STRING\033[0m'
 
