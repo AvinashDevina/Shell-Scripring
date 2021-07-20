0c131df5e7254c86ed63052c32af3004c48c5061
Avinash Devina <mail4uavi@gmail.com>
2021-07-20 06:54:26 +0530

    Modification

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -2,10 +2,13 @@
 
 echo -e "Step1 : \e[32mInstalling Nginx\e[0m"
 #yum install nginx -y &>> ~/logs/frontend.log
-#echo "Installation is Successfull"
+#echo "Installation is Successful"
 
 #echo "Starting Nginx"
 #systemctl start nginx
+#echo "Nginx is Started"
 
 #echo "Enabling Nginx Auto Start after Reboot"
 #systemctl enable nginx
+#echo "Done Enabling Nginx"
+
