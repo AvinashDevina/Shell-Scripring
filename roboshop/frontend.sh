d73684c892de773e8fe99054dccfee9879f3899f
Avinash Devina <mail4uavi@gmail.com>
2021-07-21 12:26:28 +0530

    Modification

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -2,29 +2,25 @@
 
 LOG=/tmp/roboshoplogs/frontend.log
 
-echo -n -e "\t\e[32m1. Installing Nginx\t....\e[0m"
-yum install nginx -y &>> $LOG
-if [ $? -eq 0 ]; then
+
+STATUS_CHECK(){
+	if [ $1 -eq 0 ]; then
 	echo -e "\e[33mDone\e[0m"
 else
 	echo -e "\e[33mfailed\e[0m (\e[31mPlease check the Errors in Log file at - $LOG\e[0m)"
 	exit 1
 fi
+}
+
+
+echo -n -e "\t\e[32m1. Installing Nginx\t....\e[0m"
+yum install nginx -y &>> $LOG
+STATUS_CHECK $?
 
 echo -n -e "\t\e[32m2. Starting Nginx\t....\e[0m"
 systemctl start nginx &>> $LOG
-if [ $? -eq 0 ]; then
-	echo -e "\e[33mDone\e[0m"
-else
-	echo -e "\e[33mfailed\e[0m (\e[31mPlease check the Errors in Log file at - $LOG\e[0m)"
-	exit 1
-fi
+STATUS_CHECK $?
 
 echo -n -e "\t\e[32m3. Enabling Nginx\t....\e[0m"
 systemctl enable nginx &>> $LOG
-if [ $? -eq 0 ]; then
-	echo -e "\e[33mDone\e[0m"
-else
-	echo -e "\e[33mfailed\e[0m (\e[31mPlease check the Errors in Log file at - $LOG\e[0m)"
-	exit 1
-fi
+STATUS_CHECK $?
