9144008be6969cc7d237df1d06f09a3665aefaea
Avinash Devina <mail4uavi@gmail.com>
2021-07-21 13:55:58 +0530

    Modification

--- roboshop/mongodb.sh
+++ roboshop/mongodb.sh
@@ -4,8 +4,9 @@ LOG=/tmp/roboshoplogs/mongodb.log
 source common.sh
 
 echo -n -e "\t\e[32m1. Creating MongoDB Repo\e[0m....\t\t"
+rm -f /etc/yum.repos.d/mongodb.repo -y &>>$LOG
 echo '[mongodb-org-4.2]
-name=MongoDB Repository
+name=MongoDBRepo
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
 gpgcheck=1
 enabled=1
@@ -17,7 +18,10 @@ yum install mongodb-org -y &>>$LOG
 STATUS_CHECK $?
 
 
-
+#Update Liste IP address from 127.0.0.1 to 0.0.0.0 in config file
+#Config file: /etc/mongod.conf
+#
+#
 
 #STATUS_CHECK $?
 # yum install -y mongodb-org
