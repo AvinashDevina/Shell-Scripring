bf1b404715b29f253fa38e3e6a1fa8842ed560fa
Avinash Devina <mail4uavi@gmail.com>
2021-07-21 13:14:22 +0530

    Modification

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -16,8 +16,14 @@ STATUS_CHECK $?
 echo -n -e "\t\e[32m3. Extracting & Moving Frontend Code\e[0m.... \t"
 unzip /tmp/frontend.zip &>>$LOG && mv frontend-main/* . &>>$LOG && mv static/* . &>>$LOG
 STATUS_CHECK $?
-# rm -rf frontend-master static
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+echo -n -e "\t\e[32m4. Cleaning useless Directories\e[0m.... \t\t\t"
+cd /usr/share/nginx/html && rm -rf frontend-master static &>>$LOG
+STATUS_CHECK $?
+
+echo -n -e "\t\e[32m5. Updating roboshop.conf\e[0m.... \t\t\t"
+mv localhost.conf /etc/nginx/default.d/roboshop.conf &>>$LOG
+STATUS_CHECK $?
 
 echo -n -e "\t\e[32m6. Installing Nginx\e[0m....\t\t\t\t"
 yum install nginx -y &>> $LOG
