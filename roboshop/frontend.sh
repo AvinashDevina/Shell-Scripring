23ba4fe5df6562635f4a795141928144cde73557
Avinash Devina <mail4uavi@gmail.com>
2021-07-21 12:11:37 +0530

    Modification

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -3,11 +3,11 @@
 LOG=/tmp/roboshoplogs/frontend.log
 
 echo -n -e "\t\e[32m1. Installing Nginx....... \e[0m"
-yum install ngiinx -y &>> $LOG
+yum install nginx -y &>> $LOG
 if [ $? -eq 0 ]; then
 	echo -e "\e[33mDone\e[0m"
 else
-	echo -e "\e[33mfailed\e[0m (\e[31mPlease check the Log file at - $LOG\e[0m)"
+	echo -e "\e[33mfailed\e[0m (\e[31mPlease check the Errors in Log file at - $LOG\e[0m)"
 fi
 #echo "Starting Nginx"
 #systemctl start nginx &>> $LOG
