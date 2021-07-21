0808d80f66ebf58a440e102a1b20cd35efb34175
Avinash Devina <mail4uavi@gmail.com>
2021-07-21 12:15:08 +0530

    Modification

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -8,6 +8,7 @@ if [ $? -eq 0 ]; then
 	echo -e "\e[33mDone\e[0m"
 else
 	echo -e "\e[33mfailed\e[0m (\e[31mPlease check the Errors in Log file at - $LOG\e[0m)"
+	exit 1
 fi
 #echo "Starting Nginx"
 #systemctl start nginx &>> $LOG
