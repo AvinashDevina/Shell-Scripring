b995870bca43fa2c29e685c42cc6e54e73935669
Avinash Devina <mail4uavi@gmail.com>
2021-07-21 12:44:59 +0530

    Modification

--- roboshop/common.sh
+++ roboshop/common.sh
@@ -2,7 +2,7 @@
 
 USER_ID_CHECK=$(id -u)
 if [ $USER_ID_CHECK -ne 0 ]; then
-	echo -e "\n[31m You should be a root or sudo user to run this script\e[0m"
+	echo -e "\n\e®[31m You should be a root or sudo user to run this script\e[0m"
 	exit 2
 fi
 
