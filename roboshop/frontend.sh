d93254cb4c7a7133fbb55786019393fc5d5337cb
Avinash Devina <mail4uavi@gmail.com>
2021-07-21 13:03:23 +0530

    Modification

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -4,16 +4,16 @@ LOG=/tmp/roboshoplogs/frontend.log
 source common.sh
 
 
-echo -e "\e[31m1. Cleaning Old Code "
+echo -n -e "\t\e[31m1. Cleaning Old Code "
 cd /usr/share/nginx/html && rm -rf *
 STATUS_CHECK $?
 
 
-echo -n -e "\e[31m2. Downloading Frontend Code "
+echo -n -e "\t\e[31m2. Downloading Frontend Code "
 curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>$LOG
 STATUS_CHECK $?
 
-echo -e "\e[31m3. Extracting & Moving Frontend Code "
+echo -n -e "\t\e[31m3. Extracting & Moving Frontend Code "
 unzip /tmp/frontend.zip &>>$LOG && mv frontend-main/* . &>>$LOG && mv static/* . &>>$LOG
 # rm -rf frontend-master static
 # mv localhost.conf /etc/nginx/default.d/roboshop.conf
