9bb082cad865703c1f22600f8ecb0472fea41522
Avinash Devina <mail4uavi@gmail.com>
2021-07-21 12:43:40 +0530

    Modification

--- roboshop/common.sh
+++ roboshop/common.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+USER_ID_CHECK=$(id -u)
+if [ $USER_ID_CHECK -ne 0]; then
+	echo -e "\n[31m You should be a root or sudo user to run this script\e[0m"
+	exit 2
+fi
 
 STATUS_CHECK(){
 	if [ $1 -eq 0 ]; then
@@ -8,10 +13,4 @@ else
 	echo -e "\e[33mfailed\e[0m (\e[31mPlease check the Errors in Log file at - $LOG\e[0m)"
 	exit 1
 fi
-}
-
-USER_ID_CHECK=$(id -u){
-	if [ $USER_ID_CHECK -ne 0]; then
-		echo -e "\n[31m You should be a root or sudo user to run this script\e[0m"
-		exit 2
 }
