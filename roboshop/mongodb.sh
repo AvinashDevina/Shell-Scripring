1f66dcf3435ff99486e41a05e546cee950978a9e
Avinash Devina <mail4uavi@gmail.com>
2021-07-21 13:42:30 +0530

    Modification

--- roboshop/mongodb.sh
+++ roboshop/mongodb.sh
@@ -3,7 +3,7 @@
 LOG=/tmp/roboshoplogs/mongodb.log
 source common.sh
 
-echo -n -e "\t\e[32m1. Creating MongoDB Repo\e[0m....\t\t\t\t"
+echo -n -e "\t\e[32m1. Creating MongoDB Repo\e[0m....\t\t"
 echo '[mongodb-org-4.2]
 name=MongoDB Repository
 baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/4.2/x86_64/
@@ -12,6 +12,14 @@ enabled=1
 gpgkey=https://www.mongodb.org/static/pgp/server-4.2.asc' > /etc/yum.repos.d/mongodb.repo
 STATUS_CHECK $?
 
-#echo -n -e "\t\e[32m1. Installing MongoDB\e[0m....\t\t\t\t"
-#yum install nginx -y &>> $LOG
+echo -n -e "\t\e[32m1. Installing MongoDB\e[0m....\t\t"
+yum clean all -y && yum update all -y && yum install mongodb-org -y &>>$LOG
+STATUS_CHECK $?
+
+
+
+
 #STATUS_CHECK $?
+# yum install -y mongodb-org
+# systemctl enable mongod
+# systemctl start mongod
