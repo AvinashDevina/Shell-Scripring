100cb7da74b831dbc5843aa76bf29d833ab0a702
mail4uavi@gmail.com <mail4uavi@gmail.com>
2023-01-09 22:41:44 +0530

    updating ./Shell-Scripting

--- 01.sh
+++ 01.sh
@@ -2,6 +2,9 @@
 
 echo -e "\n\033[1;31mINSTALLING NGINX\e[0m"
 yum install nginx -y &>/tmp/test.log
-if [$? == 0 ]; then
-  echo -e "\n\033[1;33mNGINX INSTALLATION IS SUCCESSFUL\e[0m"
+if [ $? -eq 0 ]
+  then
+    echo -e "\n\033[1;33mNGINX INSTALLATION IS SUCCESSFUL\e[0m"
+  else
+    echo -e "\n\033[1;31mNGINX INSTALLATION IS FAILED\e[0m"
 fi
