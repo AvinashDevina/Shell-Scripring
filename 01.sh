ce6231d5483cb66f86473dbb3d5ec0ce5bf9c063
mail4uavi@gmail.com <mail4uavi@gmail.com>
2023-01-09 22:46:28 +0530

    updating ./Shell-Scripting

--- 01.sh
+++ 01.sh
@@ -1,10 +1,12 @@
 #!/bin/zsh
-
-echo -e "\n\033[1;31mINSTALLING NGINX\e[0m"
+LOG="/tmp/test.log"
+echo -e "\n\033[1;31mInstalling Nginx\e[0m"
 yum install nginx -y &>/tmp/test.log
 if [ $? -eq 0 ]
   then
-    echo -e "\n\033[1;33mNGINX INSTALLATION IS SUCCESSFUL\e[0m"
+    echo -e "\n\033[1;33mNginx Installation is Successful\e[0m"
+    echo -e "\n\033[1;31mCheck the Log file for More Info :$LOG \e[0m"
   else
-    echo -e "\n\033[1;31mNGINX INSTALLATION IS FAILED\e[0m"
+    echo -e "\n\033[1;31mNginx Installation is failed\e[0m"
+    echo -e "\n\033[1;31mCheck the Log file for More Info :$LOG \e[0m"
 fi
